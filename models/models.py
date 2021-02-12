a2a9ddde5b89a264c4417e09698114e5efb9560d
EDallas <inmsanz89@gmail.com>
2021-02-12 10:28:55 +0000

    Funtional commit

--- models/models.py
+++ models/models.py
@@ -26,9 +26,19 @@ class StockPicking(models.Model):
 
 #    @api.onchange('actual_state')
 #    def _onchange_actual_state(self):
-#        po = self.env['purchase.order'].search([('name', 'like', self.origin)])
-#        po.write({'delivery_state' : self.actual_state})
+#        if self.origin:
+#            po =  self.env['purchase.order'].search([('name', '=', self.origin)], limit=1)
+#            po.write({'delivery_state' : "Meh"})
+
+#        if self.origin:
+#            origin_split = self.origin.split(", ")
+#            for origin in origin_split:   
+#                ## purchase order - search purchase orders
+#                po =  self.env['purchase.order'].search([('name', '=', origin)], limit=1)
+#                po.write({'internal_note' : self.internal_note})
 #
+
+
 #    @api.model
 #    def cron_previus_deliverys(self):
 #        for record in self.search([]):
@@ -39,4 +49,9 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     picking_count = fields.Integer()
-    delivery_state = fields.Char()
+    delivery_state = fields.Char(compute='_compute_delivery_state')
+
+    @api.one
+    def _compute_delivery_state(self):
+        sp = self.env['stock.picking'].search([('origin', '=', self.name)], limit=1)
+        self.delivery_state = sp.actual_state
