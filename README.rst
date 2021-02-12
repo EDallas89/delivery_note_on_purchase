50245a0e071d367e911dab0d8a6f855944161959
EDallas <inmsanz89@gmail.com>
2021-02-12 10:49:08 +0000

    Add readme, logo and version 1.0

---
+++ README.rst
@@ -0,0 +1,11 @@
+Delivery Note On Purchase
+=========================
+
+    This module add the delivery note count and state on purchase order view tree.
+
+Credits
+-------
+* Authors
+    * Aresoltec Canarias S.L.
+* Contributors
+    * Inma Sánchez
--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'author': "Aresoltec Canarias S.L.",
     'website': "http://www.aresoltec.com",
     'category': 'Purchase',
-    'version': '0.1',
+    'version': '1.0',
     'depends': ['purchase', 'stock'],
     'data': [
         'views/views.xml',
